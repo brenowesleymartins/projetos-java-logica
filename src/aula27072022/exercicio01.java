9159e9bb79ea74774deb39bac5dd32cecfacd88d
brenowesleymartins <brenoetayla2019@gmail.com>
2022-08-04 16:07:40 -0300

    ok

---
+++ src/aula27072022/exercicio01.java
@@ -0,0 +1,26 @@
+package aula27072022;
+
+public class exercicio01 {
+    // psvm + TAB = CRIA O METODO PRINCIPAL
+
+    public static void main(String[] args) {
+        // sout + TAB = CRIA A IMPRESSAOD E DADOS
+        System.out.println("Ola");
+
+        // listas de videos
+        // https://www.youtube.com/watch?v=JEAQeT7YGs4&list=PLHz_AreHm4dkI2ZdjTwZA4mPMxWTfNSpR&index=11
+
+        // https://www.youtube.com/watch?v=W9V5wtOOZHs&list=PLHz_AreHm4dkI2ZdjTwZA4mPMxWTfNSpR&index=14
+
+        // https://www.youtube.com/watch?v=xHgnlic7fj8&list=PLHz_AreHm4dkI2ZdjTwZA4mPMxWTfNSpR&index=16
+
+        // video para resolver exercicios
+        // https://www.youtube.com/watch?v=oNSrBld06qs&list=PLHz_AreHm4dkI2ZdjTwZA4mPMxWTfNSpR&index=19
+
+        // programar online pelo github
+        // https://www.gitpod.io/
+        // logar com github
+
+
+    }
+}
---
+++ src/exemplo.java
@@ -0,0 +1,13 @@
+public class exemplo {
+
+    // link atividades
+    // https://atividades-senac-gelvazio.vercel.app/atividades.html
+    // do dia 27-07-2022
+    public static void main(String[] args) {
+
+        System.out.println("mensagem na tela");
+
+
+
+    }
+}
