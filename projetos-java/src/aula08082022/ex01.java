9cb44eb33609ecd56930b9a6dcd661811ea13c17
brenowesleymartins <brenoetayla2019@gmail.com>
2022-08-11 13:39:00 -0300

    inicial

---
+++ projetos-java/src/aula08082022/ex01.java
@@ -0,0 +1,21 @@
+package aula08082022;
+
+public class ex01 {
+    // psvm + TAb
+
+    // Exercicios:
+    //
+    public static void main(String[] args) {
+        //sout + tab
+        System.out.println("Ola");
+
+        int idade = 30;
+
+        if(idade == 30){
+            System.out.println("A idade e igual a 30!");
+        } else {
+            System.out.println("A idade nao e igual a 30!");
+        }
+
+    }
+}
---
+++ projetos-java/src/aula08082022/exercicio01.java
@@ -0,0 +1,21 @@
+package aula08082022;
+import java.util.Scanner;
+public class exercicio01 {
+    // Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+    // criar 3 variaveis int e uma soma
+    // Ao final, imprima na tela se a soma de A + B é menor que C.
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner((System.in));
+
+        System.out.println("informe o valor de a:");
+        int nuA = entrada.nextInt();
+        System.out.println("informe o valor de b:");
+        int nuB = entrada.nextInt();
+        System.out.println("informe o valor de c:");
+        int nuC = entrada.nextInt();
+        if (nuA>nuB){
+
+        }
+    }
+
+}
